bd7aff4a1561c89e4c7789594d4690ec80f9406e
Dan Enman <enmand@gmail.com>
2020-10-25 16:46:54 -0300

    Add custom entc compiler with entgql templates, with generation and docs
    The custom entc compiler we use here, is to add the pre-compiled code
    generation templates from ent-contrib entgql integration with gqlgen.

---
+++ internal/ent/doc.go
@@ -0,0 +1,6 @@
+// Package ent provides schema definitions, using fields, indexes, hooks and
+// policies, and includes the generated type-safe data access layer code.
+//
+// The `gen/` directory stores all generated files, other directories are
+// packages relating to ent, or used in the generation of ent generated code.
+package ent
---
+++ internal/ent/entc.go
@@ -0,0 +1,29 @@
+// +build ignore
+
+// Package main provides a custom entc compiler, which includes the
+// ent-contrib templates for gqlgen
+package main
+
+import (
+	"log"
+
+	"github.com/facebook/ent/entc"
+	"github.com/facebook/ent/entc/gen"
+	"github.com/facebook/ent/schema/field"
+	"github.com/facebookincubator/ent-contrib/entgql"
+)
+
+// TODO: pkg should be configurable, and/or set using go.mod name
+const pkg = "github.com/enmand/dnsbl-query/internel/ent/gen/ent"
+
+func main() {
+	err := entc.Generate("./schema", &gen.Config{
+		IDType:    &field.TypeInfo{Type: field.TypeString},
+		Target:    "./gen/ent/",
+		Package:   pkg,
+		Templates: entgql.AllTemplates,
+	})
+	if err != nil {
+		log.Fatalf("running ent codegen: %v", err)
+	}
+}
