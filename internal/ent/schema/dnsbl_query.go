0a6cd5af92fe604ed02d1ed25fb1c62ab4c28ff2
Dan Enman <enmand@gmail.com>
2020-10-25 16:51:03 -0300

    Add the DNSBLQuery entity
    The DNSBLQuery entity stores query information to DNSBL services
    for an IP address. It has edges from IP addresses, and to responses.

---
+++ internal/ent/schema/dnsbl_query.go
@@ -0,0 +1,31 @@
+package schema
+
+import (
+	"github.com/enmand/dnsbl-query/internal/ent/schema/mixin"
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+)
+
+// DNSBLQuery is the edge containing query results for a given DNSBL
+// check for an IP address
+type DNSBLQuery struct {
+	ent.Schema
+}
+
+// Mixins are the field mixins for the DNSBLQuery entity
+func (DNSBLQuery) Mixins() []ent.Mixin {
+	return []ent.Mixin{
+		mixin.ID{},
+		mixin.Timed{},
+	}
+}
+
+// Edges are the entity edges for the DNSBLQuery in the graph.
+func (DNSBLQuery) Edges() []ent.Edge {
+	return []ent.Edge{
+		edge.To("responses", DNSBLResponse.Type),
+		edge.From("ip_address", IP.Type).
+			Ref("queries").
+			Unique(),
+	}
+}
