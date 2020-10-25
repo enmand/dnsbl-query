b8ff53885bf34b53408e7bfd344eebc9d371ee19
Dan Enman <enmand@gmail.com>
2020-10-25 16:52:08 -0300

    Add the DNSBLResponse entity
    The DNSBLResponse entity stores information about DNSBL service
    queries for an IP.

---
+++ internal/ent/schema/dnsbl_response.go
@@ -0,0 +1,38 @@
+package schema
+
+import (
+	"github.com/enmand/dnsbl-query/internal/ent/schema/mixin"
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+)
+
+// DNSBLResponse is the response from the DNSBL service
+type DNSBLResponse struct {
+	ent.Schema
+}
+
+// Fields are the ent field definitions for the DNSBLResponse entity
+func (DNSBLResponse) Fields() []ent.Field {
+	return []ent.Field{
+		field.String("code"),
+		field.String("description"),
+	}
+}
+
+// Mixins are the field mixins for the DNSBLResponse entity
+func (DNSBLResponse) Mixins() []ent.Mixin {
+	return []ent.Mixin{
+		mixin.ID{},
+		mixin.Timed{},
+	}
+}
+
+// Edges are the entity edges for the DNSBLResponse entity in the graph
+func (DNSBLResponse) Edge() []ent.Edge {
+	return []ent.Edge{
+		edge.From("query", DNSBLQuery.Type).
+			Ref("responses").
+			Unique(),
+	}
+}
