5cd694df6a2b74b9ada331e8d5e1c7496f4e3b76
Dan Enman <enmand@gmail.com>
2020-10-28 21:33:46 -0300

    Move common flags to `flags` pkg

--- internal/graphql/graphql.go
+++ internal/graphql/graphql.go
@@ -16,17 +16,16 @@ import (
 
 	"github.com/enmand/dnsbl-query/internal/ent/gen/ent"
 	"github.com/enmand/dnsbl-query/internal/ent/gen/ent/migrate"
+	"github.com/enmand/dnsbl-query/internal/flags"
 	"github.com/enmand/dnsbl-query/internal/graphql/internal/resolver"
 )
 
 // GraphQLFlags are the flags for the GraphQL HTTP service
 type GraphQLFlags struct {
+	flags.Database
+
 	Listen string `short:"p" long:"port" env:"PORT" description:"Port to run the server on" default:":8080"`
-
-	DatabaseURI    string `short:"u" long:"database-uri" env:"DATABASE_URI" description:"database URI to connect to" default:"file:ent?mode=memory&cache=shared&_fk=1"`
-	DatabaseDriver string `short:"s" long:"database-driver" env:"DATABASE_DRIVER" description:"Database driver to use" default:"sqlite3"`
-
-	Debug bool `long:"debug" env:"DEBUG" description:"If the server should be in debug mode"`
+	Debug  bool   `long:"debug" env:"DEBUG" description:"If the server should be in debug mode"`
 }
 
 // Flags are the configurable set of GraphQLFlags
